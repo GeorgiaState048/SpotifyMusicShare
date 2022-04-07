c2d82810516662cfc1b8db1d7ba8365c998ce140
Jonathan Laurent <jlaurent4@student.gsu.edu>
2022-04-07 16:29:58 -0400

    uses python to access the API instead of react

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,23 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable no-param-reassign */
 /* eslint-disable camelcase */
+/* eslint-disable prefer-template */
+/* eslint-disable no-unused-vars */
+
 // test
 import './App.css';
 import {
-  useEffect, React,
+  useEffect, useState, React,
 } from 'react';
 import env from 'react-dotenv';
-import UserProfile from './components/UserProfile';
-// import { FunFact } from './FunFact';
+import { Playlists } from './Info';
+// import axios from 'axios';
+// import UserProfile from './components/UserProfile';
 
 const SPOTIFY_KEY = env.CLIENT_ID; // insert your client id here from spotify
 const SPOTIFY_AUTHORIZE_ENDPOINT = 'https://accounts.spotify.com/authorize';
-const REDIRECT_URL_AFTER_LOGIN = 'http://10.250.34.208:8000/';
+const USER_ENDPOINT = 'https://api.spotify.com/v1/me';
+const REDIRECT_URL_AFTER_LOGIN = 'http://10.32.193.217:8000/';
 const SPACE_DELIMITER = '%20';
 const SCOPES = [
   'user-read-currently-playing',
@@ -35,6 +40,12 @@ const getReturnedParamsFromSpotifyAuth = (hash) => {
 };
 
 function App() {
+  const [data, setData] = useState({});
+  const [token, setToken] = useState('');
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('');
+  const [playlists, setPlaylists] = useState([]);
+
   useEffect(() => {
     if (window.location.hash) {
       const {
@@ -45,18 +56,71 @@ function App() {
       localStorage.clear();
 
       localStorage.setItem('access_token', access_token);
-      console.log('This is the App localStorage test', localStorage.getItem('access_token'));
+      setToken(access_token);
+      const newData = { token };
+      fetch('/get_access_token', {
+        method: 'POST',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify(newData),
+      });
     }
   });
+
   function handleLogin() {
     window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${SPOTIFY_KEY}&redirect_uri=${REDIRECT_URL_AFTER_LOGIN}&scope=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true`;
   }
+
+  // the json file returns a list of playlist names
+  function renderPlaylistNames(i) {
+    return <Playlists plNames={playlists[i]} />;
+  }
+  // I use map function to print each playlist
+  const myPlaylists = playlists.map((currElement, index) => (
+    <p className="rowC">
+      {renderPlaylistNames(index)}
+    </p>
+  ));
+
+  // when the get playlists button is clicked, it accesses the API endpoint
+  // i created in the get_playlists() in app.py and gets the playlist info 
+  // from that function
+  const handleGetUserPlaylists = () => {
+    const getData = async () => {
+      const response = await fetch('/get_playlists');
+      const info = await response.json();
+      console.log(info);
+      const pl_names = info[0];
+      setPlaylists(pl_names.PlaylistNames);
+    };
+    getData();
+  };
+
+  // when the get user info button is clicked, it accesses the API endpoint
+  // i created in the get_user_info() in app.py and gets the user info 
+  // from that function
+  const handleGetUserProfile = () => {
+    const getData = async () => {
+      const response = await fetch('/user_info');
+      const info = await response.json();
+      console.log(info);
+      const username = info[0];
+      const pic = info[1];
+      setName(username.Username);
+      setImage(pic.ProfilePic);
+    };
+    getData();
+  };
+
   return (
     <body>
       <h1>Click Below to Login to your Spotify Account!</h1>
       <button onClick={handleLogin}>log in to spotify</button>
       <button onClick={handleLogin}>log in as a different user</button>
-      <UserProfile />
+      <button onClick={handleGetUserProfile}>Get User Info</button>
+      <button onClick={handleGetUserPlaylists}>Get User Playlists</button>
+      {name}
+      <img src={image} alt="Profile Pic" width="500" height="600" />
+      {myPlaylists}
     </body>
   );
 }
---
+++ src/Info.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import {
+    useState, React, useRef,
+  } from 'react';
+  
+  
+export function Playlists(props) {
+    return (
+        <div>
+        Playlists: {props.plNames}
+        </div>
+    );
+}
